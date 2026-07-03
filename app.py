d9459fd7aac4b497d05294c1cc012164467a5440
Gunji Nandini <gunjinandini112@gmail.com>
2026-07-03 18:27:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import joblib
+import numpy as np
 
 app = Flask(__name__)
 
+# Load trained model
 model = joblib.load("models/credit_card_model.pkl")
 
 
@@ -19,21 +21,41 @@ def predict():
 @app.route("/result", methods=["POST"])
 def result():
 
-    income = float(request.form["income"])
-    age = float(request.form["age"])
-    employment = float(request.form["employment"])
+    try:
+        features = [
+            float(request.form["ID"]),
+            float(request.form["CODE_GENDER"]),
+            float(request.form["FLAG_OWN_CAR"]),
+            float(request.form["FLAG_OWN_REALTY"]),
+            float(request.form["CNT_CHILDREN"]),
+            float(request.form["AMT_INCOME_TOTAL"]),
+            float(request.form["NAME_INCOME_TYPE"]),
+            float(request.form["NAME_EDUCATION_TYPE"]),
+            float(request.form["NAME_FAMILY_STATUS"]),
+            float(request.form["NAME_HOUSING_TYPE"]),
+            float(request.form["DAYS_BIRTH"]),
+            float(request.form["DAYS_EMPLOYED"]),
+            float(request.form["FLAG_WORK_PHONE"]),
+            float(request.form["FLAG_PHONE"]),
+            float(request.form["FLAG_EMAIL"]),
+            float(request.form["OCCUPATION_TYPE"]),
+            float(request.form["CNT_FAM_MEMBERS"]),
+            float(request.form["Family_Size"]),
+            float(request.form["MONTHS_BALANCE"]),
+            float(request.form["STATUS"])
+        ]
 
-    prediction = model.predict([[income, age, employment]])
+        prediction = model.predict(np.array(features).reshape(1, -1))
 
-    if prediction[0] == 1:
-        output = "Credit Card Approved"
-    else:
-        output = "Credit Card Rejected"
+        if prediction[0] == 1:
+            result = "✅ Credit Card Approved"
+        else:
+            result = "❌ Credit Card Rejected"
 
-    return render_template(
-        "result.html",
-        prediction=output
-    )
+        return render_template("result.html", prediction=result)
+
+    except Exception as e:
+        return f"Error: {e}"
 
 
 if __name__ == "__main__":
